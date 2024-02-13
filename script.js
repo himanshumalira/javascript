0f5300db4418bbc3cc6766d9c6c778954ea5de8b
himanshumalira <147631977+himanshumalira@users.noreply.github.com>
2024-02-13 15:04:35 +0530

    push

---
+++ script.js
@@ -0,0 +1,93 @@
+// the hello world program
+// console.log(" Hello World!!")
+// document.write('hello world')
+
+
+//  to add two nums
+
+// const num1 = 5
+// const num2 = 6
+
+// const sum = num1 + num2
+
+//  add two num enterd by the user
+
+// const num1 = parseInt(prompt('enter first number'));
+// const num2 = parseInt(prompt('enter the second number'))
+
+// const sum = num1 + num2
+
+// console.log('the sum of ${num1} and ${num2} is ${sum}')
+
+
+
+
+// Square Root of num
+
+// const num = prompt('enter the num: ')
+// const result = Math.sqrt(num)
+// console.log(result)
+
+
+//  Area of a Triangle
+
+// const base = 14
+// const height = 15
+//  const areaOfTriangle = (base*height)/2
+
+//  console.log('Area of Triangle is ${areaOfTriangle}')
+
+
+// Swap two numbers
+
+// let a = 5
+// let b = 6
+
+// let temp;
+// temp = a
+// a = b
+// b = temp
+
+// console.log(` ${a} and ${b}`)
+//  using es6 destructuring assignment
+// let a = 5
+// let b = 7
+// [a, b] = [b, a];
+// console.log(a)
+
+
+
+// Solve Quadratic Equation
+
+// let a = 7
+// let b = 5
+// let c = 6
+
+// let discrimental = b*b - 4*a*c
+
+// let result = (-b + Math.sqrt(discrimental))/2*a
+
+// console.log(result)
+
+//  check the number is positive negative or zero
+// let num = 0
+
+// if (num > 0) {
+//     console.log("Number is Positive")
+// } else if(num < 0){
+//     console.log("Number is negative")
+// }else {
+//     console.log("Number is zero")
+// }
+
+
+//  check the number is odd even 
+
+let num = 31
+if (num%2 == 0) {
+    console.log('the number is Even')
+} else if(num%2 !== 0){
+    console.log('the number is odd')
+}else {
+    console.log('the number is zero')
+}
