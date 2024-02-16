0dca377dd8156bd66d3e5bac5864c79bdc6a96e9
himanshumalira <147631977+himanshumalira@users.noreply.github.com>
2024-02-16 23:10:44 +0530

    iffe

--- 09_iife.js
+++ 09_iife.js
@@ -1,2 +1,13 @@
 //  IIFE => Immediatly Invoked Function Expression
-//  ()();
+//  ( function (){} )();
+//  sometime we have facing problems by globel variables polusion, become we use IIFE  
+//  always use semi colon ; after ending the code 
+
+( function(){
+    console.log(`Himanshu`);
+})();
+
+( (parameter) => {
+    console.log(`PAthak ${parameter}`);
+})("Argument");
+
