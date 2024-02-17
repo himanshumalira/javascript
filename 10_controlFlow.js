155bac81c47fb795d5ca77de6c26baa37336a080
himanshumalira <147631977+himanshumalira@users.noreply.github.com>
2024-02-17 21:49:40 +0530

    control flow

---
+++ 10_controlFlow.js
@@ -0,0 +1,55 @@
+// IF condition
+
+if(true){
+    //  code execute
+}
+if(false){
+    // code does not execute
+}
+
+
+//  Switch
+
+// switch (Key){
+//     case value:
+//     break;
+//     default: 
+//     break;
+// }
+
+// const month = "April"
+
+// switch (month) {
+//     case "January": console.log(" january");
+//     break;
+//     case "Feb": console.log(" feb");
+//     break;
+//     case "MArch": console.log(" march");
+//     break;
+//     case "April": console.log(" April");
+        
+//         break;
+
+//     default: console.log("Default case match");
+//         break;
+// }
+
+
+// Falsy Value => false, 0, -0, 0n(BigInt), null, undefined, NaN, "Empty String"
+// Truthy Value => "0", " ", [], {}, function(){}
+
+
+
+//  Nullish Coalescing Operator ( ?? ): Null, Undefined
+
+// let val1 = 5 ?? 6 ( First Value Assign)
+// let val1 = null ?? 6  ( Second Value Assign)
+// let val1 = undefined ?? 6 ( Second Value Assign)
+// let val1 = undefined ?? 6 ?? 10 ( Second Value Assign)
+
+
+//  Terniary Operator ( ? )
+
+// condition ? true : false
+// const ice = 100
+// ice>= 80 ? console.log("greater than 80") : console.log("less than 80");
